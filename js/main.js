25b9fbc20ab47a4263365aee342c10a7313b794a
Rohit Kodam <74586376+Rohitkk432@users.noreply.github.com>
2022-04-09 17:07:01 +0530

    preloader test5

--- js/main.js
+++ js/main.js
@@ -116,16 +116,41 @@
     /*--------------------
        MAGNIFIC POPUP JS
        ----------------------*/
-    // 
-    function removeLoader(){
-        $( ".preloader" ).fadeOut(500, function() {
-          // fadeOut complete. Remove the loading div
-          $( ".preloader" ).remove(); //makes page more lightweight 
-        });  
-    }
-    $('body').append('<div class="preloader"><span><i class="lnr lnr-sun"></i></span></div>');
-    $(window).on('load', function(){
-      setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+    var magnifPopup = function () {
+        $('.popup').magnificPopup({
+            type: 'image',
+            removalDelay: 300,
+            mainClass: 'mfp-with-zoom',
+            gallery: {
+                enabled: true
+            },
+            zoom: {
+                enabled: true, // By default it's false, so don't forget to enable it
+
+                duration: 300, // duration of the effect, in milliseconds
+                easing: 'ease-in-out', // CSS transition easing function
+
+                // The "opener" function should return the element from which popup will be zoomed in
+                // and to which popup will be scaled down
+                // By defailt it looks for an image tag:
+                opener: function (openerElement) {
+                    // openerElement is the element on which popup was initialized, in this case its <a> tag
+                    // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                    return openerElement.is('img') ? openerElement : openerElement.find('img');
+                }
+            }
+        });
+    };
+    // Call the functions
+    magnifPopup();
+    /* Preloader Js
+    ===================*/
+    $(window).ready(function () {
+        $('.preloader').fadeOut(500);
+        /*WoW js Active
+        =================*/
+        new WOW().init({
+            mobile: false,
+        });
     });
-    
 })(jQuery);
