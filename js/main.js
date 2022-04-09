883149b33553013ca5bd259152fac09066ff5ab1
Rohit Kodam <74586376+Rohitkk432@users.noreply.github.com>
2022-04-09 16:43:13 +0530

    preloader test3

--- js/main.js
+++ js/main.js
@@ -133,11 +133,11 @@
                 // The "opener" function should return the element from which popup will be zoomed in
                 // and to which popup will be scaled down
                 // By defailt it looks for an image tag:
-                opener: function (openerElement) {
+                // opener: function (openerElement) {
                     // openerElement is the element on which popup was initialized, in this case its <a> tag
                     // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-                    return openerElement.is('img') ? openerElement : openerElement.find('img');
-                }
+                    // return openerElement.is('img') ? openerElement : openerElement.find('img');
+                // }
             }
         });
     };
