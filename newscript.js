eeddae9ed1ff73b9127aa18a5695c40eb3421a69
Rohitkk432 <74586376+Rohitkk432@users.noreply.github.com>
2022-06-28 21:36:21 +0530

    added testimonials carousel

--- newscript.js
+++ newscript.js
@@ -26,3 +26,70 @@ hamburger.addEventListener('click', () => {
     CloseIcon.classList.toggle('hidden');
 });
 
+// carousel
+
+$(document).ready(function(){
+
+    $(".gallerycarousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        autoplayHoverPause: true,
+        center: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            1180: {
+                items: 3,
+            },
+        },
+    });
+    $(".testcarousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        autoplayHoverPause: true,
+        center: true,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding: 0,
+            },
+            550:{
+                items: 1,
+                stagePadding: 100,
+            },
+            650:{
+                items: 1,
+                stagePadding: 150,
+            },
+            750:{
+                items: 1,
+                stagePadding: 200,
+            },
+            850:{
+                items: 1,
+                stagePadding: 250,
+            },
+            950:{
+                items: 1,
+                stagePadding: 300,
+            },
+            1100: {
+                items: 3,
+                stagePadding: 20,
+            },
+            1200:{
+                items: 3,
+                stagePadding: 70,
+            }
+        },
+    });
+    
+});
